42c20cf9c60a47c1ec7cfdd0f61b82701c24417a
Ivlev Denis <ivlevdenis.ru@gmail.com>
2015-03-10 14:08:38 +0300

    add path observer

--- main.py
+++ main.py
@@ -8,29 +8,35 @@ from kivy.lang import Builder
 from kivy.adapters.listadapter import ListAdapter
 from kivy.uix.listview import ListView, ListItemButton, ListItemLabel
 from kivy.properties import (StringProperty)
+
 data = []
+
 
 class VideoListButton(ListItemButton):
     filename = StringProperty('')
+
 
 args_converter = lambda row_index, rec: {'text': rec['text'],
                                          'size_hint_y': None,
                                          'height': 25,
                                          'filename': rec['filename']}
 
-list_adapter = ListAdapter(data=data,
+list_adapter = ListAdapter(data=[],
                            args_converter=args_converter,
                            cls=VideoListButton,
                            selection_mode='single',
                            allow_empty_selection=False)
 
+
 class SimpleVideoPlayer(App):
+
     def __init__(self):
         self.root = Builder.load_file('main.kv')
         self.root.ids.path.bind(text=self.change_path)
         self.pattern = '*.mp4'
         self.root.ids.filelist.adapter = list_adapter
-        self.root.ids.filelist.adapter.bind(on_selection_change=self.change_selection)
+        self.root.ids.filelist.adapter.bind(
+            on_selection_change=self.change_selection)
 
     def built(self):
         return self.root
@@ -50,6 +56,7 @@ class SimpleVideoPlayer(App):
         self.root.ids.filelist.adapter.data = [{'text': os.path.basename(f),
                                                 'is_selected': False,
                                                 'filename': f} for f in self.filelist]
+
 
 if __name__ == '__main__':
     SimpleVideoPlayer().run()
